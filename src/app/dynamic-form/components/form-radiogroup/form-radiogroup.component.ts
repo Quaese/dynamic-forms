79b3b7da411528d30540996d9b1f4aad8f6e5697
Quaese <quaese@gmx.net>
2019-01-11 10:57:17 +0100

    Dynamic Form -  Review Changes

--- src/app/dynamic-form/components/form-radiogroup/form-radiogroup.component.ts
+++ src/app/dynamic-form/components/form-radiogroup/form-radiogroup.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+import { FieldConfig } from './../../models/field-config.interface';
 
 @Component({
   selector: 'form-radiogroup',
   templateUrl: './form-radiogroup.component.html',
   styleUrls: ['./form-radiogroup.component.less']
 })
-export class FormRadiogroupComponent implements OnInit {
+export class FormRadiogroupComponent {
+  config: FieldConfig;
+  group: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor() {}
 }
